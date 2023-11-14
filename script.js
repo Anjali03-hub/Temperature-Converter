e5c26ce9bb7fc1e4ea1a7df124047db7a78dc698
Anjali03-hub <parmalanjali@gmail.com>
2023-11-14 15:16:26 +0530

    Converter

---
+++ script.js
@@ -0,0 +1,33 @@
+const celsiusElem = document.querySelector("#celsius");
+const degree = document.querySelector("#degree");
+const convertBtn = document.querySelector("#convert-btn");
+const tempType = document.querySelector("#temp-type");
+document.getElementById('clear').onclick = clearForm;
+
+window.addEventListener("load", () => {
+  degree.value = "";
+  celsiusElem.innerHTML = "";
+});
+
+convertBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  convertToCelsius();
+});
+
+function convertToCelsius() {
+  let inputValue = degree.value;
+
+if (tempType.value === "fahrenheit") {
+    const FahrenheitToCelsius = (inputValue - 32) * (5 / 9);
+    celsiusElem.innerHTML = `${FahrenheitToCelsius.toFixed(3)} &deg;c`;
+  } else if (tempType.value === "kelvin") {
+    const KelvinToCelsius = inputValue - 273.15;
+    celsiusElem.innerHTML = `${KelvinToCelsius.toFixed(3)} &deg;c`;
+  }
+}
+
+function clearForm() {
+    document.querySelector("#degree").value='';
+    document.querySelector("#temp-type").value='';
+    document.querySelector("#celsius").value='';  
+} 
